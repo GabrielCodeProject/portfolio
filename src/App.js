6fa2381245ae4cec3e0da3a12796c922dffa36ee
Gabriel Lavigueur <Gabrielprivermsg@gmail.com>
2022-02-16 11:30:12 -0500

    test add image

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import './App.css';
+import picture from './img/victor.jpg'
 function App() {
   return (
     <div className="App">
-      <p>Big boi programmer, formation programmation jeux video</p>
+      <h1>Big boi programmeur</h1>
+      <p>formation programmation jeux video</p>
       <p>Experience CodeBox and work with daddy as a internship</p>
+      <img src={picture} alt="Big boi" />
     </div>
   );
 }
