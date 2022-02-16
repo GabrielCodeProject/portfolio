28024503b064e5c386e76cc37a41d7ed18ca87bf
Gabriel Lavigueur <Gabrielprivermsg@gmail.com>
2022-02-16 11:19:27 -0500

    remove testing element

--- src/App.js
+++ src/App.js
@@ -4,11 +4,7 @@ import profileImage from './img/agathe.jpg';
 function App() {
   return (
     <div className="App">
-      <h1>TA Soeur</h1>
-      <h2>Agathe</h2>
       <p>Game designer, im very good at making art design and game design :)</p>
-      <img src={profileImage} alt="Agathe" />
-
     </div>
   );
 }
