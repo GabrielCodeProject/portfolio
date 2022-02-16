900984b3238e9473de0f57691ba7bc6855d1b43b
Gabriel Lavigueur <Gabrielprivermsg@gmail.com>
2022-02-16 14:40:55 -0500

    add my name

--- src/container/App.js
+++ src/container/App.js
@@ -20,9 +20,9 @@ function App() {
 						</div>
 						<div class="col-8 col-7-large col-12-medium">
 							<header>
-								<h1>Hi. I'm <strong>Jane Doe</strong>.</h1>
+								<h1>Hi. I'm <strong>Gabriel Lavigueur</strong>.</h1>
 							</header>
-							<p>And this is <strong>Miniport</strong>, a free, fully responsive HTML5 site template designed by <a href="http://twitter.com/ajlkn">AJ</a> for <a href="http://html5up.net">HTML5 UP</a> &amp; released under the <a href="http://html5up.net/license">CCA license</a>.</p>
+							<p>And this is my<strong>Portfolio</strong>.</p>
 							<a href="#work" class="button large scrolly">Learn about what I do</a>
 						</div>
 					</div>
@@ -182,7 +182,7 @@ function App() {
 			<script src="assets/js/breakpoints.min.js"></script>
 			<script src="assets/js/util.js"></script>
 			<script src="assets/js/main.js"></script>
-      
+
       <h1>Big boi programmeur</h1>
       <p>formation programmation jeux video</p>
       <p>Experience CodeBox and work with daddy as a internship</p>
