264a35eaf7d6a4ed1475d744aec52ab529f947c9
Gabriel Lavigueur <Gabrielprivermsg@gmail.com>
2022-02-16 10:50:38 -0500

    modification app.js affichage

--- src/App.js
+++ src/App.js
@@ -4,20 +4,6 @@ import profileImage from './img/agathe.jpg';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
       <h1>TA Soeur</h1>
       <h2>Agathe</h2>
       <img src={profileImage} alt="Agathe" />
