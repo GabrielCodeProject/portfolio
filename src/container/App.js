3129821805141bed2e535577d855a23936fc587f
Gabriel Lavigueur <Gabrielprivermsg@gmail.com>
2022-02-16 12:29:47 -0500

    Refactoring la structure du projet

--- src/container/App.js
+++ src/container/App.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import '../css/App.css';
-import picture from 'img/victor.jpg'
 function App() {
   return (
     <div className="App">
       <h1>Big boi programmeur</h1>
       <p>formation programmation jeux video</p>
       <p>Experience CodeBox and work with daddy as a internship</p>
-      <img src={picture} alt="Big boi" />
+      <img src='img/victor.jpg' alt="Big boi" />
       <footer>
         <p>Lien utile pour un github page</p>
         <a href='https://github.com/gitname/react-gh-pages' >tutorial</a>
