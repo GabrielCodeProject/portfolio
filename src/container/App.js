46ba477169b904e35415f3f316e896a6b165983d
Gabriel Lavigueur <Gabrielprivermsg@gmail.com>
2022-02-16 21:05:28 -0500

    ajout des photo

--- src/container/App.js
+++ src/container/App.js
@@ -80,7 +80,7 @@ function App() {
 						</div>
 						<div class="col-4 col-6-medium col-12-small">
 							<article class="box style2">
-								<a href="#" class="image featured"><img src="images/pic02.jpg" alt="" /></a>
+								<a href="#" class="image featured"><img src="gab.jpg" alt="" /></a>
 								<h3><a href="#">Musik</a></h3>
 								<p>Ornare nulla proin odio consequat.</p>
 								<a href='https://github.com/GabrielCodeProject/Musik' >Lien github</a>
@@ -89,7 +89,7 @@ function App() {
 						</div>
 						<div class="col-4 col-6-medium col-12-small">
 							<article class="box style2">
-								<a href="#" class="image featured"><img src="images/pic03.jpg" alt="" /></a>
+								<a href="#" class="image featured"><img src="./images/gab.jpg" alt="" /></a>
 								<h3><a href="#">Memoire</a></h3>
 								<p>Ornare nulla proin odio consequat.</p>
 								<a href='https://github.com/GabrielCodeProject/Memoire' >Lien github</a>
