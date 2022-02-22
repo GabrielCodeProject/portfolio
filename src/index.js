605c2252b1300b42a40374ce6e3838fac235c2a8
Gabriel Lavigueur <Gabrielprivermsg@gmail.com>
2022-02-22 16:52:54 -0500

    add picture

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './css/index.css'
 import './css/main.css'
-import './test.html'
 import App from './container/App'
 import reportWebVitals from './buildInFile/reportWebVitals'
 
