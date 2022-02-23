fa2edfae808ef4525350e388728eae8dfebdaec7
Gabriel Lavigueur <Gabrielprivermsg@gmail.com>
2022-02-22 20:13:43 -0500

    page de test container et responsiveness

--- src/container/App.js
+++ src/container/App.js
@@ -23,207 +23,137 @@ function App () {
 
         <article id='top' class='wrapper style1'>
           <div class='container'>
-            <div class='row'>
-              <div class='col-4 col-5-large col-12-medium'>
-                <span class='image fit'>
-                  <img src='images/gab.jpg' alt='' />
-                </span>
-              </div>
-              <div class='col-8 col-7-large col-12-medium'>
-                <header>
-                  <h1>
-                    Salut! Je suis <strong>Gabriel Lavigueur</strong>.
-                  </h1>
-                </header>
-                <p>
-                  Je suis un Développeur Full-Stack. J'ai complété mes études en
-                  Programmation et Technologie internet afin de me procurer les
-                  compétences nécessaires pour performer dans un domaine qui me
-                  fascine. Je suis quelqu'un de <strong>Passionné</strong>,{' '}
-                  <strong>Autodidacte</strong> et <strong>Polyvalent</strong>.
-                  <br /> Et voici mon <strong>Portfolio</strong>.
-                </p>
-                <a href='./GabrielCV2022.pdf' class='button small' download>
-                  Mon CV
-                </a>
-              </div>
-            </div>
-          </div>
-        </article>
+            <div class='projets-master-container'>
+              <header>
+                <h2>Mes projets</h2>
+              </header>
+              <div class='projets-list-container'>
+                <div class='project-container-detail'>
+                  <img
+                    class='img-projet'
+                    src='css/images/journeecarriereshowcase.png'
+                    alt=''
+                  />
+                  <div class='projet-resumer'>
+                    <h3>Journée Carrière</h3>
+                    <p>
+                      <strong>Site Web</strong> permettant de créer et de faire
+                      la gestion d'une journée carrière. Il contient aussi une{' '}
+                      <strong>application mobile</strong> développée en{' '}
+                      <strong>Java</strong>, qui permet aux utilisateurs
+                      inscrits de se connecter. L'application permet de voir ton
+                      horaire, ta liste de meeting avec les détails de celle-ci
+                      et le profil de certain utilisateur selon son type de
+                      connexion.
+                    </p>
+                    <a
+                      class='github-link'
+                      href='https://github.com/GabrielCodeProject/JourneeCarriere'
+                    >
+                      Lien github
+                    </a>
+                  </div>
+                </div>
 
-        <article id='work' class='wrapper style2'>
-          <div class='container'>
-            <header>
-              <h2>Mes compétences</h2>
-            </header>
-            <div class='row aln-center'>
-              <div class='col-4 col-6-medium col-12-small'>
-                <section class='box style1'>
-                  <span class='icon featured fa-comments' />
-                  <h3>Intégration Web</h3>
-                  <p>
-                    Intégration <strong>HTML / CSS</strong> qui respecte les{' '}
-                    <a href='lien vers les standards'>standards du Web</a>.
-                    Design <strong>responsive</strong> et{' '}
-                    <strong>dynamique</strong>, compatible sur plusieurs formats
-                    tels que <span>mobile, tablette et desktop.</span>
-                  </p>
-                </section>
-              </div>
+                <div class='project-container-detail'>
+                  <img
+                    class='img-projet'
+                    src='css/images/musikshowcase.png'
+                    alt=''
+                  />
+                  <div class='projet-resumer'>
+                    <h3>Musik</h3>
+                    <p>
+                      <strong>Site Web</strong> static d'apparence
+                      <strong>E-Boutique</strong> sur le thème de la vente de
+                      cours de musique et materiel de musique tel que diverses
+                      Guitares. Utilisant uniquement <strong>HTML/CSS</strong>{' '}
+                      avec un <strong>Design Responsive</strong> sur plusieurs
+                      platformes <strong>mobiles, tablettes et desktops</strong>
+                      .
+                    </p>
+                    <a href='https://github.com/GabrielCodeProject/Musik'>
+                      Lien github
+                    </a>
+                  </div>
+                </div>
 
-              <div class='col-4 col-6-medium col-12-small'>
-                <section class='box style1'>
-                  <span class='icon solid featured fa-camera-retro' />
-                  <h3>Back-End</h3>
-                  <p>
-                    Connection avec un <strong>API REST</strong>, Traitement de
-                    données reçu par une base de données{' '}
-                    <span>"MySQL, PostgreSQL, MariaDB, SQLite"</span> et un
-                    système d'authentification.
-                  </p>
-                </section>
-              </div>
+                <div class='project-container-detail'>
+                  <img
+                    class='img-projet'
+                    src='css/images/memoireShowCaseX.png'
+                    alt=''
+                  />
+                  <div class='projet-resumer'>
+                    <h3>Mémoire</h3>
+                    <p>
+                      Jeux de Mémoire <strong>Web</strong> qui a pour objectif
+                      de trouver chaque pair de carte. Le jeu contient un
+                      système de pointage et de difficulté. Cette page utilise
+                      uniquement <strong>HTML/CSS</strong> et{' '}
+                      <a href='lien vers javascript'>Javascript</a>
+                    </p>
+                    <a href='https://github.com/GabrielCodeProject/Memoire'>
+                      Lien github
+                    </a>
+                  </div>
+                </div>
 
-              <div class='col-4 col-6-medium col-12-small'>
-                <section class='box style1'>
-                  <span class='icon featured fa-thumbs-up' />
-                  <h3>Mobile</h3>
-                  <p>
-                    Développement d'application <strong>mobile</strong> avec{' '}
-                    <strong>Android Studio</strong>
-                    utilisant <strong>Java</strong>. De plus, j'apprends aussi
-                    le framework <a href='lien vers flutter'>flutter</a> qui
-                    utilise le langage dart que l'on peut comparer à{' '}
-                    <a href='lien vers javascript'>javascript</a>.
-                  </p>
-                </section>
-              </div>
-            </div>
+                <div class='project-container-detail'>
+                  <img
+                    class='img-projet'
+                    src='css/images/gainztimeshowcase.png'
+                    alt=''
+                  />
+                  <div class='projet-resumer'>
+                    <h3>GainzTime</h3>
+                    <p>
+                      <strong>Application Mobile</strong>, développée en{' '}
+                      <strong>Java</strong> avec <strong>Android Studio</strong>
+                      , permettant de remplir un formulaire profil de l'athlète,
+                      suivre sa progression physique, son entraînement du jour,
+                      et de créer un plan aléatoire.
+                    </p>
+                    <a href='https://github.com/GabrielCodeProject/GainzTime'>
+                      Lien github
+                    </a>
+                  </div>
+                </div>
 
-            <footer>
-              <a href='#portfolio' class='button large scrolly'>
-                Voir mes projets.
-              </a>
-            </footer>
-          </div>
-        </article>
-
-        <article id='portfolio' class='wrapper style3'>
-          <div class='container'>
-            <header>
-              <h2>Mes projets</h2>
-            </header>
-            <div class='row'>
-              <div class='col-4 col-6-medium col-12-small'>
-                <article class='box style2'>
-                  <a href='#' class='image featured'>
-                    <img src='images/journeecarriereshowcase.png' alt='' />
-                  </a>
-                  <h3>Journée Carrière</h3>
-                  <p>
-                    <strong>Site Web</strong> permettant de créer et de faire la
-                    gestion d'une journée carrière. Il contient aussi une{' '}
-                    <strong>application mobile</strong> développée en{' '}
-                    <strong>Java</strong>, qui permet aux utilisateurs inscrits
-                    de se connecter. L'application permet de voir ton horaire,
-                    ta liste de meeting avec les détails de celle-ci et le
-                    profil de certain utilisateur selon son type de connexion.
-                  </p>
-                  <a href='https://github.com/GabrielCodeProject/JourneeCarriere'>
-                    Lien github
-                  </a>
-                </article>
-              </div>
-
-              <div class='col-4 col-6-medium col-12-small'>
-                <article class='box style2'>
-                  <a href='#' class='image featured'>
-                    <img src='images/musikshowcase.png' alt='' />
-                  </a>
-                  <h3>Musik</h3>
-                  <p>
-                    <strong>Site Web</strong> static d'apparence{' '}
-                    <strong>E-Boutique</strong> sur le thème de la vente de
-                    cours de musique et materiel de musique tel que diverses
-                    Guitares. Utilisant uniquement <strong>HTML/CSS</strong>{' '}
-                    avec un <strong>Design Responsive</strong> sur plusieurs
-                    platformes <strong>mobiles, tablettes et desktops</strong>.
-                  </p>
-                  <a href='https://github.com/GabrielCodeProject/Musik'>
-                    Lien github
-                  </a>
-                </article>
-              </div>
-
-              <div class='col-4 col-6-medium col-12-small'>
-                <article class='box style2'>
-                  <a href='#' class='image featured'>
-                    <img src='images/memoireShowCaseX.png' alt='' />
-                  </a>
-                  <h3>Mémoire</h3>
-                  <p>
-                    Jeux de Mémoire <strong>Web</strong> qui a pour objectif de
-                    trouver chaque pair de carte. Le jeu contient un système de
-                    pointage et de difficulté. Cette page utilise uniquement{' '}
-                    <strong>HTML/CSS</strong> et{' '}
-                    <a href='lien vers javascript'>Javascript</a>
-                  </p>
-                  <a href='https://github.com/GabrielCodeProject/Memoire'>
-                    Lien github
-                  </a>
-                </article>
-              </div>
-
-              <div class='col-4 col-6-medium col-12-small'>
-                <article class='box style2'>
-                  <a href='#' class='image featured'>
-                    <img src='images/gainztimeshowcase.png' alt='' />
-                  </a>
-                  <h3>GainzTime</h3>
-                  <p>
-                    <strong>Application Mobile</strong>, développée en{' '}
-                    <strong>Java</strong> avec <strong>Android Studio</strong>,
-                    permettant de remplir un formulaire profil de l'athlète,
-                    suivre sa progression physique, son entraînement du jour, et
-                    de créer un plan aléatoire.
-                  </p>
-                  <a href='https://github.com/GabrielCodeProject/GainzTime'>
-                    Lien github
-                  </a>
-                </article>
-              </div>
-
-              <div class='col-4 col-6-medium col-12-small'>
-                <article class='box style2'>
-                  <a href='#' class='image featured'>
-                    <img src='images/stargrazeshowcase.png' alt='' />
-                  </a>
-                  <h3>StarGraze</h3>
-                  <p>
-                    Jeu développé lors du{' '}
-                    <a href='lien si fonctionnel'>Game Jam Montréal</a> en
-                    janvier 2020 à l'ÉTS sur la thématique “Repair”. Utilisant{' '}
-                    <strong>Unity</strong> et <strong>C#</strong> lors du
-                    développement, nous avons conçu en équipe de cinq, StarGraze
-                    !{' '}
-                    <span>
-                      "Pris dans une chaîne d'asteroid, tu devras réparer ton
-                      vaisseau qui se fait heurter par les asteroids. Grâce au
-                      toolbar d'outils, tu pourras réparer ton vaisseau et
-                      survivre."
-                    </span>
-                  </p>
-                  <a
-                    href='https://gabrielcodeproject.itch.io/spacegaze?secret=N234BvIkw2xGpcHt897BwabrU'
-                    class='button small scrolly'
-                  >
-                    Essayer la démo
-                  </a>
-                  <br></br>
-                  <a href='https://github.com/GabrielCodeProject/StarGraze'>
-                    Lien github
-                  </a>
-                </article>
+                <div class='project-container-detail'>
+                  <img
+                    class='img-projet'
+                    src='css/images/stargrazeshowcase.png'
+                    alt=''
+                  />
+                  <div class='projet-resumer'>
+                    <h3>StarGraze</h3>
+                    <p>
+                      Jeu développé lors du{' '}
+                      <a href='lien si fonctionnel'>Game Jam Montréal</a> en
+                      janvier 2020 à l'ÉTS sur la thématique “Repair”. Utilisant{' '}
+                      <strong>Unity</strong> et <strong>C#</strong> lors du
+                      développement, nous avons conçu en équipe de cinq,
+                      StarGraze !{' '}
+                      <span>
+                        "Pris dans une chaîne d'asteroid, tu devras réparer ton
+                        vaisseau qui se fait heurter par les asteroids. Grâce au
+                        toolbar d'outils, tu pourras réparer ton vaisseau et
+                        survivre."
+                      </span>
+                    </p>
+                    <a
+                      href='https://gabrielcodeproject.itch.io/spacegaze?secret=N234BvIkw2xGpcHt897BwabrU'
+                      class='button small scrolly'
+                    >
+                      Essayer la démo
+                    </a>
+                    <br></br>
+                    <a href='https://github.com/GabrielCodeProject/StarGraze'>
+                      Lien github
+                    </a>
+                  </div>
+                </div>
               </div>
             </div>
 
