733119e803f578df65f5fc694acf016ba8ea47c7
Gabriel Lavigueur <Gabrielprivermsg@gmail.com>
2022-03-02 17:47:59 -0500

    responsiveness complet

--- src/component/projet-detail-component.js
+++ src/component/projet-detail-component.js
@@ -2,7 +2,9 @@ import React from 'react'
 
 const ProjetDetailComponent = ({ title, description, imgPath }) => (
   <div class='project-container-detail'>
-    <img class='img-projet' src={imgPath} alt='' />
+    <div class='image-container-projet'>
+      <img class='img-projet' src={imgPath} alt='' />
+    </div>
     <div class='projet-resumer'>
       <h3 class='grand-titre-mobile'>{title}</h3>
       <p>{description}</p>
